40eebc6c6fdf191f1c3c7263679629efbe8fede1
Tony V <vaninanton@gmail.com>
2025-07-09 21:04:06 +0500

    feat: обновить точки в points.json с новыми описаниями и названиями

--- src/helpers/CreatePopupForFeature.js
+++ src/helpers/CreatePopupForFeature.js
@@ -1,10 +1,10 @@
 import FeatureTypes from './FeatureTypes'
 
 function createPopupContent(feature) {
-    const { type, name, description } = feature.properties ?? {};
-    const coordinates = feature.geometry.coordinates;
-    const isPoint = feature.geometry.type === 'Point';
-    const isMobile2GisType = (window.innerWidth <= 800) ? 'scooter' : 'pedestrian';
+    const { type, name, description } = feature.properties ?? {}
+    const coordinates = feature.geometry.coordinates
+    const isPoint = feature.geometry.type === 'Point'
+    const isMobile2GisType = window.innerWidth <= 800 ? 'scooter' : 'pedestrian'
 
     const content = [
         type && FeatureTypes[type] ? `<span class="font-semibold">${FeatureTypes[type]?.name}</span> ` : '',
@@ -12,12 +12,16 @@ function createPopupContent(feature) {
         description ? `<div>${description}</div>` : '',
         isPoint ? '<div class="maps-links">' : '',
         isPoint ? '<h3>Построить маршрут:</h3>' : '',
-        isPoint ? `<a href="https://yandex.ru/maps/?rtext=~${coordinates[1]},${coordinates[0]}&rtt=sc" target="_blank">Яндекс.Карты</a> ` : '',
-        isPoint ? `<a href="https://2gis.kz/directions/tab/${isMobile2GisType}/points/|${coordinates[0]},${coordinates[1]}" target="_blank">2GIS</a>` : '',
+        isPoint
+            ? `<a href="https://yandex.ru/maps/?rtext=~${coordinates[1]},${coordinates[0]}&rtt=sc" target="_blank">Яндекс.Карты</a> `
+            : '',
+        isPoint
+            ? `<a href="https://2gis.kz/directions/tab/${isMobile2GisType}/points/|${coordinates[0]},${coordinates[1]}" target="_blank">2GIS</a>`
+            : '',
         isPoint ? '</div>' : '',
-    ];
+    ]
 
-    return content.join('');
+    return content.join('')
 }
 
 export default function (feature, layer) {
