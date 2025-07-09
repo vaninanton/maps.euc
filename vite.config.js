547730afae011bce2b3e84b9543ac3d9244dddb5
Tony V <vaninanton@gmail.com>
2025-07-09 18:49:20 +0500

    feat: add manual chunking in build configuration

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,20 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+    plugins: [vue()],
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('@geoman-io')) {
+                        return '@geoman-io'
+                    }
+                    if (id.includes('leaflet')) {
+                        return 'leaflet'
+                    }
+                    return
+                },
+            },
+        },
+    },
 })
