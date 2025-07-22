66a70a81c4f276060b18ed75559841f102d01785
Tony V <vaninanton@gmail.com>
2025-07-22 15:30:47 +0500

    fix: Исправлены отступы для tooltip

--- src/helpers/BlueIcon.js
+++ src/helpers/BlueIcon.js
@@ -5,7 +5,7 @@ export default new L.Icon({
     iconUrl: blueMarker,
     shadowUrl: shadowMarker,
     iconSize: [25, 41],
-    iconAnchor: [6, 41],
+    iconAnchor: [12, 41],
     popupAnchor: [1, -34],
     shadowSize: [41, 41],
 })
