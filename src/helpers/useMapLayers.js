6a84e8bd38f661ee71e03f4075832ecc8b958059
Tony V <vaninanton@gmail.com>
2025-07-22 11:51:45 +0500

    fix: навел красоту в useMapLayers

--- src/helpers/useMapLayers.js
+++ src/helpers/useMapLayers.js
@@ -12,44 +12,29 @@ import velojolAlmaty from '../assets/velojol/almaty.json'
 export const createPointsLayer = () =>
     L.geoJSON(pointsGeojson, {
         pmIgnore: true,
-        pointToLayer: function (feature, latlng) {
-            return L.marker(latlng, {
-                icon: blueIcon,
-            })
-        },
-        onEachFeature: createPopupForFeature,
+        pointToLayer: (_, latlng) => L.marker(latlng, { icon: blueIcon }),
+        onEachFeature: (feature, layer) => createPopupForFeature(feature, layer, 'point'),
     })
 
 export const createSocketsLayer = () =>
     L.geoJSON(socketsGeojson, {
         pmIgnore: true,
-        pointToLayer: function (feature, latlng) {
-            return L.marker(latlng, {
-                icon: greenIcon,
-            })
-        },
-        onEachFeature: createPopupForFeature,
+        pointToLayer: (_, latlng) => L.marker(latlng, { icon: greenIcon }),
+        onEachFeature: (feature, layer) => createPopupForFeature(feature, layer, 'socket'),
     })
 
 export const createRoutesLayer = () =>
     L.geoJSON(routesGeojson, {
         pmIgnore: true,
-        style: {
-            color: '#f25824',
-            weight: 2.5,
-        },
-        onEachFeature: createPopupForFeature,
+        style: { color: '#f25824', weight: 2.5 },
+        onEachFeature: (feature, layer) => createPopupForFeature(feature, layer, 'route'),
     })
 
-export const createVeloLayer = () => {
+export const createBikelanesLayer = () => {
     const veloGeoJson = velojol2geojson(velojolAlmaty, ['alm84', 'alm85', 'alm86', 'alm89'])
     return L.geoJSON(veloGeoJson, {
         pmIgnore: true,
-        style: {
-            color: 'green',
-            weight: 3,
-            dashArray: '6, 6',
-        },
-        onEachFeature: createPopupForFeature,
+        style: { color: 'green', weight: 3, dashArray: '6, 6' },
+        onEachFeature: (feature, layer) => createPopupForFeature(feature, layer, 'bikelane'),
     })
 }
